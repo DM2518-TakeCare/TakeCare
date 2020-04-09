fa03d6a79dfac9e807627aae477b5106c7986364
ellaklara <ellaklara.westerlund@gmail.com>
2020-04-09 21:27:39 +0200

    Add divided view component

---
+++ src/components/divided-view/divided-view.tsx
@@ -0,0 +1,57 @@
+import React, { FC, ReactNode } from 'react'
+import { StyleSheet, View } from 'react-native'
+import Svg, { Path } from 'react-native-svg'
+import { paperTheme } from '../../theme/paper-theme'
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        height: '100%',
+        width: '100%',
+    },
+    background: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    svg: {
+        zIndex: -1,
+    },
+    childrenCont: {
+        display: 'flex',
+        justifyContent: 'center',
+        height: '50%',
+        padding: '5%',
+    },
+    upper: {
+        position: 'absolute',
+    }
+});
+
+type DividedViewProps = {
+    upper: ReactNode,
+    lower: ReactNode,
+    reverse?: Boolean
+}
+
+const  DividedView: FC<DividedViewProps> = (props) => {
+  return (
+    <View style={styles.container}>
+        <View style={{...styles.background, backgroundColor: props.reverse? paperTheme.colors.background : paperTheme.colors.primary}}>
+            <Svg style={styles.svg} width={'100%'} height={'50%'} viewBox='0 0 360 351' fill='none' {...props}>
+                <Path
+                    d='M271.58 313.777C220.458 363.025 139.542 363.025 88.4202 313.777L-38.9611 191.064C-124.512 108.648 -66.1723 -36 52.6188 -36L307.381 -36C426.172 -36 484.512 108.648 398.961 191.064L271.58 313.777Z'
+                    fill={props.reverse? paperTheme.colors.primary : paperTheme.colors.background}
+                />
+            </Svg>
+            <View style={{...styles.childrenCont, ...styles.upper}}>
+                {props.upper}
+            </View>
+            <View style={styles.childrenCont}>
+                {props.lower}
+            </View>
+        </View>
+    </View>
+  )
+}
+
+export default DividedView
--- src/pages/playground-page.tsx
+++ src/pages/playground-page.tsx
@@ -3,11 +3,22 @@ import { StyleSheet, View, Text } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList, RoutePropsHelper } from '../router';
 import { Center } from '../components/center';
+import DividedView from '../components/divided-view/divided-view';
+import { Button } from 'react-native-paper';
+import { paperTheme } from '../theme/paper-theme'
 
 export default function PlaygroundPage({navigation, route}:RoutePropsHelper<'Playground'>) {
+    const button = <Button mode='contained' onPress={() => {}}>Hello</Button>
     return (
-        <Center>
-            <Text>Test</Text>
-        </Center>
+        <>
+            <DividedView 
+                upper={button} 
+                lower={
+                    <Text style={{color: paperTheme.colors.onPrimary}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas faucibus ex non scelerisque fermentum. Sed fringilla ac nunc eu molestie. Morbi lobortis congue magna, vitae consectetur velit fringilla semper. Suspendisse varius nulla a ornare cursus. Fusce sapien purus, pellentesque ut gravida nec, condimentum sed nibh. Etiam a ante ipsum. Nam id augue tristique, vehicula diam id, aliquet augue. In sagittis iaculis tortor in auctor. Nullam vel quam metus. Duis luctus in diam ut dapibus. Duis suscipit vestibulum mauris. Nam imperdiet justo non magna mollis mollis. Aliquam gravida massa pellentesque nulla vulputate, sed euismod mi tempus.
+                    </Text>
+                }
+            />
+        </>
     );
 }
