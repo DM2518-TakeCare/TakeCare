f6bac985ed9477855e031c6301616a448deb903f
ellaklara <ellaklara.westerlund@gmail.com>
2020-04-10 07:18:45 +0200

    Update divided view to use center component

--- src/components/divided-view/divided-view.tsx
+++ src/components/divided-view/divided-view.tsx
@@ -1,7 +1,9 @@
 import React, { FC, ReactNode } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, TouchableWithoutFeedback } from 'react-native'
 import Svg, { Path } from 'react-native-svg'
 import { paperTheme } from '../../theme/paper-theme'
+import { Center } from '../center';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const styles = StyleSheet.create({
     container: {
@@ -13,12 +15,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center'
     },
-    svg: {
-        zIndex: -1,
-    },
     childrenCont: {
-        display: 'flex',
-        justifyContent: 'center',
         height: '50%',
         padding: '5%',
     },
@@ -37,17 +34,21 @@ const  DividedView: FC<DividedViewProps> = (props) => {
   return (
     <View style={styles.container}>
         <View style={{...styles.background, backgroundColor: props.reverse? paperTheme.colors.background : paperTheme.colors.primary}}>
-            <Svg style={styles.svg} width={'100%'} height={'50%'} viewBox='0 0 360 351' fill='none' {...props}>
+            <Svg width={'100%'} height={'50%'} viewBox='0 0 360 351' fill='none' {...props}>
                 <Path
                     d='M271.58 313.777C220.458 363.025 139.542 363.025 88.4202 313.777L-38.9611 191.064C-124.512 108.648 -66.1723 -36 52.6188 -36L307.381 -36C426.172 -36 484.512 108.648 398.961 191.064L271.58 313.777Z'
                     fill={props.reverse? paperTheme.colors.primary : paperTheme.colors.background}
                 />
             </Svg>
             <View style={{...styles.childrenCont, ...styles.upper}}>
-                {props.upper}
+                <Center>
+                    {props.upper}
+                </Center>
             </View>
             <View style={styles.childrenCont}>
-                {props.lower}
+                <Center>
+                    {props.lower}
+                </Center>
             </View>
         </View>
     </View>
