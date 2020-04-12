035f110e063f07102145dd6633950b4b682fe5c0
= <adamsmajl@gmail.com>
2020-04-12 15:25:54 +0200

    Remove button example in playground

--- src/pages/playground-page.tsx
+++ src/pages/playground-page.tsx
@@ -14,56 +14,8 @@ export default function PlaygroundPage({navigation, route}:RoutePropsHelper<'Pla
     const [toggleOff, setToggleOff] = useState(true);
 
     return (
-        <DividedView 
-            upper={
-                <Center>
-                    <View>
-                        <Button onPress={() => {}}>
-                            I am a normal button
-                        </Button>
-                        <Divider style={{margin: 10}} />
-                        <Button disabled={true} onPress={() => {}}>
-                            I am a disabled button
-                        </Button>
-                        <Divider style={{margin: 10}} />
-                        <Button color={paperTheme.colors.error} onPress={() => {}}>
-                            I am a different color
-                        </Button>
-                        <Divider style={{margin: 10}} />
-                        <Button size='big' onPress={() => {}}>
-                            I am a big button
-                        </Button>
-                        <Divider style={{margin: 10}} />
-                        <Button toggleOff={toggleOff} onPress={() => {setToggleOff(!toggleOff)}}>
-                            Toggle me
-                        </Button>
-                    </View>
-                </Center>
-            }
-            lower={
-                <View style={{
-                    flex: 1,
-                    alignItems: 'center',
-                    justifyContent: 'flex-end',
-                    }}>
-                    <Button 
-                        expandHorizontal={false} 
-                        size='big' 
-                        disabled={false} 
-                        onPress={() => {}}
-                        color={paperTheme.colors.background}>
-                        Not expanded
-                    </Button>
-                    <Divider style={{margin: 10}} />
-                    <Button 
-                        expandHorizontal={true} 
-                        size='big' 
-                        disabled={false} 
-                        onPress={() => {}}
-                        color={paperTheme.colors.background}>
-                        Expanded
-                    </Button>
-                </View>
-            }/>
+        <Center>
+            <Text>Hello</Text>
+        </Center>
     );
 }
