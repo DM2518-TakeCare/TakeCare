e2a0cd7751b5c730e55fb460b84527c7cef0d7a4
Gil Laufer <gillaufer@Gils-MacBook-Pro.local>
2020-04-19 00:55:35 +0200

    added loading spinner, ex. in playground

--- src/pages/playground-page.tsx
+++ src/pages/playground-page.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Text } from 'react-native';
 import { RoutePropsHelper } from '../router';
 import { Center } from '../components/center';
+import { Spinner } from '../components/loading-spinner';
 
 export default function PlaygroundPage({navigation, route}:RoutePropsHelper<'Playground'>) {
     return (
         <Center>
             <Text>Hello</Text>
+            <Spinner />
         </Center>
     );
 }
