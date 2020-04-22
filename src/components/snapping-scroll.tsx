16a6fc89e6dfb4f6779ebce5e63f1f9f34e4cf84
= <adamsmajl@gmail.com>
2020-04-22 14:15:03 +0200

    Made snapping scroll a FC and fixed bugs

--- src/components/snapping-scroll.tsx
+++ src/components/snapping-scroll.tsx
@@ -1,118 +1,128 @@
-import React, { useState, useRef, ReactNode, Component, createRef } from 'react';
+import React, { useState, useRef, ReactNode, Component, createRef, useEffect } from 'react';
 import {View, Text, Animated, NativeSyntheticEvent, NativeScrollEvent, ScrollView, TextInput } from 'react-native';
 
 interface SnappingScrollProps {
     scrollItems: ReactNode[],
     scrollItemsHeight: number,
+    activeScrollItem?: number | null,
     onScrollItemSnap: (index: number | null) => void,
 }
 
-export class SnappingScroll extends Component<SnappingScrollProps, {animatedScrollValue: Animated.Value}> {
+export const SnappingScroll: React.FC<SnappingScrollProps> = (props) => {
 
-    currentScroll: number = 0;
-    scrollController: React.RefObject<typeof Animated.ScrollView> = createRef();
+    let currentScroll: number = 0;
+    const snappingAnimationMaxShrink = 0.95;
+    const snappingAnimationMaxOpacity = 0.6;
+    const snappingAnimationFade = 80;
 
-    constructor(props: SnappingScrollProps) {
-        super(props)
-        this.state = {
-            animatedScrollValue: new Animated.Value(0)
+    const [animatedScrollValue] = useState(new Animated.Value(0));
+    const [scrollByUser, setScrollByUser] = useState(true);
+    const scrollRef = useRef<typeof Animated.ScrollView>(null);
+
+    useEffect(() => {
+        const activeScrollItem = props.activeScrollItem;
+        if (activeScrollItem !== null && activeScrollItem !== undefined) {
+            setScrollByUser(false);
+            scrollRef.current?.getNode().scrollTo({
+                y: activeScrollItem * props.scrollItemsHeight
+            });
         }
+    }, [props.activeScrollItem])
+
+    const getItemScrollPos = (elementIndex: number) => {
+        return elementIndex * props.scrollItemsHeight;
     }
 
-    getItemScrollPos(elementIndex: number) {
-        return elementIndex * this.props.scrollItemsHeight;
-    }
-
-    getItemIndexFromScrollPos(scrollPos: number) {
-        for (let i = 0; i < this.props.scrollItems.length; i++) {
-            if (this.getItemScrollPos(i) === scrollPos) {
+    const getItemIndexFromScrollPos = (scrollPos: number) => {
+        for (let i = 0; i < props.scrollItems.length; i++) {
+            if (getItemScrollPos(i) === scrollPos) {
                 return i;
             }
         }
         return null;
     }
 
-    render() {
-        const snappingAnimationMaxShrink = 0.95;
-        const snappingAnimationMaxOpacity = 0.6;
-        const snappingAnimationFade = 80;
-        
-        return (
-            <View>
-                <Animated.ScrollView
-                    // Seem to be an error in the type definitions, works but throws an error.
-                    // We can not currently control the scroll view without getting an error :(
-                    // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/42806
-                    ref={this.scrollController}
-                    showsVerticalScrollIndicator={false}
-                    decelerationRate={0}
-                    snapToInterval={this.props.scrollItemsHeight}
-                    snapToAlignment={"start"}
-                    contentContainerStyle={{
-                        paddingBottom: '50%',
-                        paddingTop: this.props.scrollItemsHeight * 0.5
-                    }}
-                    scrollEventThrottle={16}
-                    onScroll={
-                        // Some weird React Native stuff to get the scroll value correctly
-                        // Documentation can be found here: https://reactnative.dev/docs/animated#event
-                        Animated.event(
-                            [{nativeEvent: { 
-                                    contentOffset: { 
-                                        y: this.state.animatedScrollValue
-                                    } 
+    return (
+        <View>
+            <Animated.ScrollView
+                // Seem to be an error in the type definitions, works but throws an error.
+                // We can not currently control the scroll view without getting an error :(
+                // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/42806
+                ref={scrollRef}
+                showsVerticalScrollIndicator={false}
+                decelerationRate={0}
+                snapToInterval={props.scrollItemsHeight}
+                snapToAlignment={"start"}
+                contentContainerStyle={{
+                    paddingBottom: '50%',
+                    paddingTop: props.scrollItemsHeight * 0.5,
+                    paddingHorizontal: 10
+                }}
+                scrollEventThrottle={16}
+                onScroll={
+                    // Some weird React Native stuff to get the scroll value correctly
+                    // Documentation can be found here: https://reactnative.dev/docs/animated#event
+                    Animated.event(
+                        [{nativeEvent: { 
+                                contentOffset: { 
+                                    y: animatedScrollValue
                                 } 
-                            }],{
-                                listener: (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-                                    this.currentScroll = event.nativeEvent.contentOffset.y;
-                                },
-                                useNativeDriver: true,
+                            } 
+                        }],{
+                            listener: (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+                                currentScroll = event.nativeEvent.contentOffset.y;
                             },
-                        )
+                            useNativeDriver: true,
+                        },
+                    )
+                }
+                onTouchMove={() => {
+                    setScrollByUser(true);
+                }}
+                onMomentumScrollEnd={() => {
+                    if (scrollByUser) {
+                        const itemIndex = getItemIndexFromScrollPos(currentScroll);
+                        props.onScrollItemSnap(itemIndex);
                     }
-                    onMomentumScrollEnd={() => {
-                        const itemIndex = this.getItemIndexFromScrollPos(this.currentScroll);
-                        this.props.onScrollItemSnap(itemIndex);
-                    }}
-                    >
-                        {
-                            this.props.scrollItems.map((item, index) => {
+                }}
+                >
+                    {
+                        props.scrollItems.map((item, index) => {
 
-                                // Creating a base intervall for the scroll animations
-                                const scrollPos = this.getItemScrollPos(index);
-                                const snappingAnimationBase = Animated.subtract(this.state.animatedScrollValue, scrollPos).interpolate({
-                                    inputRange: [-snappingAnimationFade, 0, snappingAnimationFade],
-                                    outputRange: [0, 1, 0],
-                                    extrapolate: 'clamp',
-                                });
-    
-                                const scaleAnimation = snappingAnimationBase.interpolate({
-                                    inputRange: [0, 1],
-                                    outputRange: [snappingAnimationMaxShrink, 1],
-                                });
-    
-                                const opacityAnimation = snappingAnimationBase.interpolate({
-                                    inputRange: [0, 1],
-                                    outputRange: [snappingAnimationMaxOpacity, 1],
-                                });
-    
-                                return (
-                                    <Animated.View key={index} style={{
-                                        transform: [
-                                            {scale: scaleAnimation}
-                                        ],
-                                        opacity: opacityAnimation,
-                                        alignItems: 'center',
-                                        justifyContent: 'center',
-                                        height: this.props.scrollItemsHeight, 
-                                    }}>
-                                        {item}
-                                    </Animated.View>
-                                )
-                            })
-                        }
-                </Animated.ScrollView>
-            </View>
-        );
-    }
+                            // Creating a base intervall for the scroll animations
+                            const scrollPos = getItemScrollPos(index);
+                            const snappingAnimationBase = Animated.subtract(animatedScrollValue, scrollPos).interpolate({
+                                inputRange: [-snappingAnimationFade, 0, snappingAnimationFade],
+                                outputRange: [0, 1, 0],
+                                extrapolate: 'clamp',
+                            });
+
+                            const scaleAnimation = snappingAnimationBase.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [snappingAnimationMaxShrink, 1],
+                            });
+
+                            const opacityAnimation = snappingAnimationBase.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [snappingAnimationMaxOpacity, 1],
+                            });
+
+                            return (
+                                <Animated.View key={index} style={{
+                                    transform: [
+                                        {scale: scaleAnimation}
+                                    ],
+                                    opacity: opacityAnimation,
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    height: props.scrollItemsHeight, 
+                                }}>
+                                    {item}
+                                </Animated.View>
+                            )
+                        })
+                    }
+            </Animated.ScrollView>
+        </View>
+    );
 }
