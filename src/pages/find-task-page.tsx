d10cbb9e70d1237587b4071c59322990b261da63
= <adamsmajl@gmail.com>
2020-04-22 14:15:24 +0200

    Add find task page skeleton

--- src/pages/find-task-page.tsx
+++ src/pages/find-task-page.tsx
@@ -1,14 +1,166 @@
-import React, { Props } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { Props, useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, Text, Dimensions } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList, RoutePropsHelper } from '../router';
 import { Center } from '../components/center';
-import { Button } from 'react-native-paper';
+import MapView, { PROVIDER_GOOGLE, LatLng } from 'react-native-maps';
+import TakeCareMap, { TakeCareMapMarker, TakeCareMapHandles } from '../components/map';
+import { TouchableWithoutFeedback, PinchGestureHandler, PanGestureHandler, ScrollView } from 'react-native-gesture-handler';
+import { ContentPadding } from '../components/content-padding';
+import { Button } from '../components/button';
+import { MaterialIcons } from '@expo/vector-icons';
+import { SnappingScroll } from '../components/snapping-scroll';
+import { Card } from 'react-native-paper';
+
+const findTaskStyle = StyleSheet.create({
+    mapContainer: {
+        position: 'relative'
+    },
+    mapControlls: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+    },
+    taskCard: {
+        width: '100%',
+        marginVertical: 5,
+        flex: 1,
+    },
+    taskCardContent: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+interface Task {
+    name: string,
+    coordinates: LatLng 
+}
 
 export default function FindTaskPage({navigation, route}:RoutePropsHelper<'FindTask'>) {
+    const [radius, setRadius] = useState(100);
+    const [followUserLocation, setFollowUserLocation] = useState(false);
+    const [userLocation, setUserLocation] = useState<LatLng | null>(null)
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [activeTaskIndex, setActiveTaskIndex] = useState<number | null>(0);
+
+    const mapRef = useRef<TakeCareMapHandles>(null);
+
+    useEffect(() => {
+        setTasks(searchForNewTasks());
+    }, []);
+
+    const searchForNewTasks = () => {
+        return [
+            {
+                name: 'Task 1',
+                coordinates: {latitude: 59.347647, longitude: 18.072340}
+            },
+            {
+                name: 'Task 2',
+                coordinates: {latitude: 59.347747, longitude: 18.072640}
+            },
+            {
+                name: 'Task 3',
+                coordinates: {latitude: 59.347447, longitude: 18.072340}
+            },
+            {
+                name: 'Task 4',
+                coordinates: {latitude: 59.347847, longitude: 18.073640}
+            },
+            {
+                name: 'Task 5',
+                coordinates: {latitude: 59.347247, longitude: 18.076540}
+            },
+        ]
+    }
+
+    const generateTasksCards = () => {
+        return tasks.map(task => {
+            return (
+                <Card style={findTaskStyle.taskCard}>
+                    <View style={findTaskStyle.taskCardContent}>
+                        <Text>
+                            {task.name}
+                        </Text>
+                    </View>
+                </Card>
+            );
+        });
+    }
+
+    const generateMarkers = (): TakeCareMapMarker[] => {
+        return tasks.map(task => {
+            return {
+                coordinates: task.coordinates
+            }
+        })
+    }
+
     return (
-        <Center>
-            <Text>Find task</Text>
-        </Center>
+        <View style={{flex: 2}}>
+            <View style={{flex: 1}}>
+                <TakeCareMap
+                    ref={mapRef}
+                    // TODO: This should be changed to an overview of Sweden / The Earth
+                    // until we get the users geolocation. Is currently KTH for testing
+                    initialMapRegion={{
+                        latitude: 59.347647,
+                        longitude: 18.072440,
+                        latitudeDelta: 0.01,
+                        longitudeDelta: 0.01,
+                    }}
+                    // mapRef={mapRef}
+                    activeMarkerIndex={activeTaskIndex}
+                    markers={generateMarkers()}
+                    followUser={followUserLocation}
+                    circle={{
+                        coordinates: userLocation ?? {
+                            latitude: 59.347647,
+                            longitude: 18.072440,
+                        },
+                        radius: radius,
+                    }}
+                    onPanDrag={() => {
+                        if (followUserLocation) {
+                            setFollowUserLocation(false);
+                        }
+                        console.log('Map dragged');
+                    }}
+                    onUserLocationChange={(location) => {
+                        setUserLocation(location);
+                    }}
+                    onMarkerPressed={taskIndex => {
+                        setActiveTaskIndex(taskIndex);
+                    }}
+                />
+                <View style={findTaskStyle.mapControlls}>
+                    <ContentPadding>
+                        <Button expandHorizontal={false} toggleOff={!followUserLocation} onPress={() => setFollowUserLocation(!followUserLocation)}>
+                            <MaterialIcons size={25} name="my-location"/>
+                        </Button>
+                    </ContentPadding>
+                </View>
+            </View>
+
+            {/* Placeholder for snapping sheet */}
+            <View style={{flex: 0.5}}>
+                <SnappingScroll
+                    activeScrollItem={activeTaskIndex}
+                    onScrollItemSnap={index => {
+                        setActiveTaskIndex(index);
+                        if (index !== null) {
+                            setFollowUserLocation(false);
+                            mapRef.current?.goToMarker(index);
+                        }
+                    }}
+                    scrollItemsHeight={80}
+                    scrollItems={
+                        generateTasksCards()
+                    }
+                    />
+            </View>
+        </View>
     );
 }
