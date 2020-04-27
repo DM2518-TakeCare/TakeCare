96bb6957c5a3d2f7cc97f85c091f0699fb4d028b
eckernas <eckernas@kth.se>
2020-04-27 16:29:17 +0200

    Remove from playground

--- src/pages/playground-page.tsx
+++ src/pages/playground-page.tsx
@@ -3,18 +3,12 @@ import { Text, View } from 'react-native';
 import { RoutePropsHelper } from '../router';
 import { Center } from '../components/center';
 import { ContentPadding } from '../components/content-padding';
-import BottomSheet from 'reanimated-bottom-sheet'
-import { HeaderBottomSheet } from '../components/header-bottom-sheet';
 
 export default function PlaygroundPage({ navigation, route }: RoutePropsHelper<'Playground'>) {
     return (
         <ContentPadding>
             <Center>
                <Text>Hello</Text>
-               <BottomSheet
-                snapPoints = {[450, 300, 0]}
-                renderContent = {() => <Text>Hej</Text>}
-                renderHeader = {() => <HeaderBottomSheet><Text>Hej</Text></HeaderBottomSheet>}></BottomSheet>
             </Center>
         </ContentPadding>
     );
