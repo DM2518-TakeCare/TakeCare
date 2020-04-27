09997b3957a42ed1e948128b127211f2ea7a8a11
= <adamsmajl@gmail.com>
2020-04-27 20:29:42 +0200

    Make the divided content work on bigger screens

--- src/components/divided-view/divided-view.tsx
+++ src/components/divided-view/divided-view.tsx
@@ -1,19 +1,15 @@
 import React, { FC, ReactNode } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ViewStyle } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
 import { paperTheme } from '../../theme/paper-theme';
+import { transform } from '@babel/core';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    background: {
-        position: 'absolute',
-        height: '100%',
-        width: '100%',
-    },
     dividedCont: {
-       zIndex: -1
+        width: '100%',
     },
     upperCont: {
         flex: 0.7,
@@ -21,7 +17,7 @@ const styles = StyleSheet.create({
     },
     lowerCont: {
         flex: 1,
-        padding: 25
+        padding: 25,
     }
 });
 
@@ -31,28 +27,32 @@ type DividedViewProps = {
     reverse?: Boolean
 }
 
-const  DividedView: FC<DividedViewProps> = (props) => {
-  return (
-    <View style={{...styles.container,  backgroundColor: props.reverse? paperTheme.colors.background : paperTheme.colors.primary}}>
-        <View style={styles.background}>
-            <View style={{...styles.upperCont, backgroundColor: props.reverse? paperTheme.colors.primary : paperTheme.colors.background}}>
-                {props.upper}     
+const DividedView: FC<DividedViewProps> = (props) => {
+    return (
+        <View style={{ 
+                ...styles.container, 
+                backgroundColor: props.reverse ? paperTheme.colors.background : paperTheme.colors.primary 
+            }}>
+            <View style={{ 
+                    ...styles.upperCont, 
+                    backgroundColor: props.reverse ? paperTheme.colors.primary : paperTheme.colors.background 
+                }}>
+                {props.upper}
             </View>
             <View style={styles.dividedCont}>
-            <Svg width={'100%'} height={'133'} preserveAspectRatio='xMinYMin' viewBox='0 0 359.57 122.05' fill='none' {...props}>
-                        <Path
-                            d='M360,234.6l-88.42,85.18a132,132,0,0,1-183.16,0L.36,234.54Z'
-                            fill={props.reverse? paperTheme.colors.primary : paperTheme.colors.background}
-                            transform='translate(-0.21 -234.67)'
-                        />
-            </Svg>
+                <Svg preserveAspectRatio="none" width={'100%'} height={130} viewBox='0 0 359.57 122.05' fill='none'>
+                    <Path
+                        d='M360,234.6l-88.42,85.18a132,132,0,0,1-183.16,0L.36,234.54Z'
+                        fill={props.reverse ? paperTheme.colors.primary : paperTheme.colors.background}
+                        transform='translate(-0.21 -234.67)'
+                    />
+                </Svg>
             </View>
             <View style={styles.lowerCont}>
                 {props.lower}
-            </View>       
+            </View>
         </View>
-    </View>
-  )
+    )
 }
 
 export default DividedView
