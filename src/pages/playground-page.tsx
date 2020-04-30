c5be0b88fdc94e6ef857f8b0a59b55f56e45984e
= <adamsmajl@gmail.com>
2020-04-30 13:05:10 +0200

    Add homepage & new playground

--- src/pages/playground-page.tsx
+++ src/pages/playground-page.tsx
@@ -3,12 +3,41 @@ import { Text, View } from 'react-native';
 import { RoutePropsHelper } from '../router';
 import { Center } from '../components/center';
 import { ContentPadding } from '../components/content-padding';
+import { Button, Divider } from 'react-native-paper';
 
 export default function PlaygroundPage({ navigation, route }: RoutePropsHelper<'Playground'>) {
     return (
         <ContentPadding>
             <Center>
-               <Text>Hello</Text>
+                <View>
+                    <Button mode="contained" onPress={() => {
+                        navigation.navigate('CreateTask');
+                    }}>To Create Task</Button>
+                    <Divider style={{margin: 10}}/>
+                    <Button mode="contained" onPress={() => {
+                        navigation.navigate('TaskAccepted');
+                    }}> To Accepted task</Button>
+                    <Divider style={{margin: 10}}/>
+                    <Button mode="contained" onPress={() => {
+                        navigation.navigate('FindTask');
+                    }}>To Find task</Button>
+                    <Divider style={{margin: 10}}/>
+                    <Button mode="contained" onPress={() => {
+                        navigation.navigate('Register');
+                    }}>To Register</Button>
+                    <Divider style={{margin: 10}}/>
+                    <Button mode="contained" onPress={() => {
+                        navigation.navigate('Home');
+                    }}>To Home</Button>
+                    <Divider style={{margin: 10}}/>
+                    <Button mode="contained" onPress={() => {
+                        navigation.navigate('Tasks');
+                    }}>To Tasks</Button>
+                    <Divider style={{margin: 10}}/>
+                    <Button mode="contained" onPress={() => {
+                        navigation.navigate('TaskCompleted');
+                    }}>To Task completed</Button>
+                </View>
             </Center>
         </ContentPadding>
     );
