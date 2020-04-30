c05267a7eee396cdfe967604c81acd2146762406
= <adamsmajl@gmail.com>
2020-04-30 22:28:48 +0200

    Add task-card component for map page

--- src/pages/playground-page.tsx
+++ src/pages/playground-page.tsx
@@ -3,12 +3,21 @@ import { Text, View } from 'react-native';
 import { RoutePropsHelper } from '../router';
 import { Center } from '../components/center';
 import { ContentPadding } from '../components/content-padding';
+import TaskCard from '../components/task-card';
 
 export default function PlaygroundPage({ navigation, route }: RoutePropsHelper<'Playground'>) {
     return (
         <ContentPadding>
             <Center>
-               <Text>Hello</Text>
+                <View style={{height: 80}}>
+                    <TaskCard
+                        distance={123}
+                        iconName="email"
+                        owner="Göran Sjögren"
+                        tag="Mail"
+                        onPress={() => {}}
+                    />
+                </View>
             </Center>
         </ContentPadding>
     );
