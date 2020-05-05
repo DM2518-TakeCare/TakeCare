5da21497a09ead95b668a12fee7684c4b3b2bf89
= <adamsmajl@gmail.com>
2020-05-05 22:09:51 +0200

    Remove test code from playground

--- src/pages/playground-page.tsx
+++ src/pages/playground-page.tsx
@@ -46,24 +46,6 @@ export default function PlaygroundPage({ navigation, route }: RoutePropsHelper<'
                     <Button mode="contained" onPress={() => {
                         navigation.navigate('HelpDetails');
                     }}>To Help Details</Button>
-                    <Divider style={{margin: 10}}/>
-                    <Button mode="contained" onPress={async () => {
-                        console.log('Starting new query');
-                        // await TaskModel.addNewTask(
-                        //     'ownerID2', 
-                        //     ['Mail'], 
-                        //     'This is an description', 
-                        //     {latitude: 59.219546, longitude: 17.873433},
-                        //     [{productName: 'Milk', amount: '2l'}, {productName: 'Pasta', amount: '1kg'}]
-                        // );
-                        await TaskModel.completeTask(
-                            'TwyCoAfkKV4h4dnAH5VR', 
-                            // 'Some random helper'
-                        );
-                        console.log('Query done, selecting new data');
-                        const result = await TaskModel.getNearbyTasks({latitude: 59.208335, longitude: 17.871182}, 2);
-                        console.log(result);
-                    }}>Firebase</Button>
                 </View>
             </Center>
         </ContentPadding>
